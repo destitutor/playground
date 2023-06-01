d521f2ce39b21aa008afa33f0e6612f4b239dab5
BINARYBARD <binarybard@pm.me>
2023-06-01 18:50:52 +0900

    Update HelloDecorator.java

--- src/main/java/tobyspring/helloboot/HelloDecorator.java
+++ src/main/java/tobyspring/helloboot/HelloDecorator.java
@@ -14,6 +14,6 @@ public class HelloDecorator implements HelloService {
 
     @Override
     public String sayHello(String name) {
-        return "*" + helloService.sayHello(name) + "*";
+        return "&" + helloService.sayHello(name) + "&";
     }
 }
