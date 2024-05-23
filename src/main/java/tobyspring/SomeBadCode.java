2f3dd904e39fa20bb4f79175a4ad2a1b9bf53604
destitutor <binarybard@pm.me>
2024-05-23 18:18:33 +0900

    feat: 그냥 추가

--- src/main/java/tobyspring/SomeBadCode.java
+++ src/main/java/tobyspring/SomeBadCode.java
@@ -4,7 +4,7 @@ import java.sql.Date;
 
 public class SomeBadCode {
     void badCode() {
-        Date date = new Date(2020, 1, 4);
+        Date date = new Date(2020, 1, 5);
         System.out.println(date);
     }
 }
